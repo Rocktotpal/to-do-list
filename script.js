6a82d3ca871372c8a692348c5db13b4b618d3408
Rocktotpal Mahanta <mahantarocktotpal@gmail.com>
2026-07-31 23:09:24 +0600

    Experimenting with todos
    A experimental task, checkbox and delete button added for understanding the items to be manipulated in DOM and basic styling is done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const date = document.getElementById("date");
+const taskField = document.querySelector(".task-field");
+const btnAdd = document.querySelector(".add");
+const toDoList = document.querySelector(".todos-list");
 
 const now = new Date();
 const options = {
@@ -10,3 +13,10 @@ const options = {
   year: "numeric",
 };
 date.textContent = new Intl.DateTimeFormat("en-IN", options).format(now);
+
+// const task = `
+// <li>
+// `;
+btnAdd.addEventListener("click", function () {
+  //   toDoList.insertAdjacentHTML();
+});
