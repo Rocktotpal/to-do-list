ec5306925f0e827107c3ce9b75971c8150e07745
Rocktotpal Mahanta <mahantarocktotpal@gmail.com>
2026-08-21 07:43:12 +0600

    Complete todo app functionality

--- script.js
+++ script.js
@@ -22,31 +22,15 @@ const options = {
 date.textContent = new Intl.DateTimeFormat("en-IN", options).format(now);
 
 toDoList.innerHTML = "";
-// let currentTask = 0;
 let taskArr = [];
 let nextId = 1;
 let currentFilter = "all";
 
 if (taskArr.length === 0) emptyState.classList.remove("hidden");
 
-const createTask = function (el) {
-  // const task =
-  //   el.completed === false
-  //     ? `
-  //   <li class="todo" data-id="${el.id}">
-  //   <input type="checkbox" class="checkbox" />
-  //   <div class="task"><p>${el.text}</p></div>
-  //   <button class="delete"><i class="fa-solid fa-x delete"></i></button>
-  //   </li>
-  //   `
-  //     : `
-  //   <li class="todo" data-id="${el.id}">
-  //   <input type="checkbox" class="checkbox" />
-  //   <div class="task"><p class="task-completed">${el.text}</p></div>
-  //   <button class="delete"><i class="fa-solid fa-x delete"></i></button>
-  //   </li>
-  //   `;
+// Task creation
 
+const createTask = function (el) {
   // Refactored 1: avoid repeated HTML
 
   const checkboxDecider =
@@ -65,23 +49,7 @@ const createTask = function (el) {
   toDoList.insertAdjacentHTML("beforeend", task);
 };
 
-// const displayActiveTask = function () {
-//   const activeTasks = taskArr.filter((task) => task.completed === false);
-
-//   toDoList.innerHTML = "";
-//   activeTasks.forEach(function (el) {
-//     createTask(el);
-//   });
-// };
-
-// const displayCompletedTask = function () {
-//   const completedTasks = taskArr.filter((task) => task.completed === true);
-
-//   toDoList.innerHTML = "";
-//   completedTasks.forEach(function (el) {
-//     createTask(el);
-//   });
-// };
+// Empty state handling functions
 
 const showEmptyState = function () {
   emptyState.classList.remove("hidden");
@@ -90,6 +58,8 @@ const showEmptyState = function () {
 const closeEmptyState = function () {
   emptyState.classList.add("hidden");
 };
+
+// Task Display function
 
 const displayTask = function () {
   toDoList.innerHTML = "";
@@ -111,6 +81,8 @@ const displayTask = function () {
   const decider = activeTasks.length > 1 ? "items" : "item";
   itemCount.textContent = `${activeTasks.length} ${decider} left`;
 };
+
+// Task addition
 
 const addTask = function () {
   if (taskField.value.trim() === "") {
@@ -154,6 +126,8 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+// Modal window handlers for empty input
+
 const closeModal = function () {
   showModal.classList.add("hidden");
   overlay.classList.add("hidden");
@@ -169,6 +143,8 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+// Delete operation handler
+
 toDoList.addEventListener("click", function (e) {
   if (e.target.classList.contains("delete")) {
     const taskToBedelete = Number(e.target.closest(".todo").dataset.id);
@@ -181,6 +157,8 @@ toDoList.addEventListener("click", function (e) {
   }
 });
 
+// Filter button handlers
+
 filters.addEventListener("click", function (e) {
   if (e.target.classList.contains("filter")) {
     const [...btnFilter] = e.target.parentElement.children;
@@ -192,20 +170,10 @@ filters.addEventListener("click", function (e) {
     // Refactored 2: displayTask is called based on currentFilter, no separate calling reqd.
 
     displayTask();
-
-    // if (e.target.dataset.state === "all") {
-    //   displayTask();
-    // }
-
-    // if (e.target.dataset.state === "active") {
-    //   displayActiveTask();
-    // }
-
-    // if (e.target.dataset.state === "completed") {
-    //   displayCompletedTask();
-    // }
   }
 });
+
+// Checkbox Handler
 
 toDoList.addEventListener("click", function (e) {
   if (e.target.classList.contains("checkbox")) {
@@ -222,148 +190,11 @@ toDoList.addEventListener("click", function (e) {
   }
 });
 
+// Completed tasks cleared
+
 clearCompleted.addEventListener("click", function () {
   taskArr = taskArr.filter((el) => {
     if (el.completed === false) return el;
   });
   displayTask();
 });
-
-// toDoList.addEventListener("click", function (e) {
-//   if (e.target.classList.contains("checkbox")) {
-//     if (!e.target.classList.contains("checkbox-clicked")) {
-//       e.target.classList.add("checkbox-clicked");
-//       e.target
-//         .closest(".todo")
-//         .querySelector("p")
-//         .classList.add("task-completed");
-//       const completedId = Number(e.target.closest(".todo").dataset.id);
-
-//       taskArr.forEach(function (task) {
-//         if (task.id === completedId) {
-//           task.completed = true;
-//         }
-//       });
-//     } else {
-//       e.target.classList.remove("checkbox-clicked");
-//       e.target
-//         .closest(".todo")
-//         .querySelector("p")
-//         .classList.remove("task-completed");
-
-//       const completedId = Number(e.target.closest(".todo").dataset.id);
-
-//       taskArr.forEach(function (task) {
-//         if (task.id === completedId) {
-//           task.completed = false;
-//         }
-//       });
-//     }
-//   }
-// });
-
-/*
-
-// function to display all tasks
-
-const displayTask = function () {
-  taskArr.forEach(function (el) {
-    const task = `
-    <li class="todo">
-    <input type="checkbox" class="checkbox" />
-    <div class="task"><p>${el}</p></div>
-    <button class="delete"><i class="fa-solid fa-x delete"></i></button>
-    </li>
-    `;
-
-    toDoList.insertAdjacentHTML("beforeend", task);
-  });
-};
-
-// function for adding a task
-
-const addTask = function () {
-  toDoList.innerHTML = "";
-
-  if (taskField.value.trim() === "") {
-    showModal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    return;
-  }
-
-  taskArr.push(taskField.value);
-
-  displayTask();
-
-  // reset input field
-
-  taskField.value = "";
-  taskField.blur();
-
-  //  updating items left
-
-  currentTask++;
-
-  itemCount.textContent = `${currentTask} items left`;
-};
-
-btnAdd.addEventListener("click", function () {
-  emptyState.classList.add("hidden");
-  addTask();
-});
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (taskField.value.trim() === "") {
-    showModal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    return;
-  }
-    emptyState.classList.add("hidden");
-    addTask();
-    }
-});
-
-
-
-
-document.querySelector(".todos-list").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  if (e.target.classList.contains("delete")) {
-    const taskToBedelete = e.target
-      .closest(".todo")
-      .querySelector("p").textContent;
-
-    const index = taskArr.findIndex((task) => task === taskToBedelete);
-
-    taskArr.splice(index, 1);
-    toDoList.innerHTML = "";
-
-    displayTask();
-
-    //  updating items left
-
-    currentTask--;
-
-    itemCount.textContent = `${currentTask} items left`;
-
-    if (currentTask === 0) {
-      emptyState.classList.remove("hidden");
-    }
-  }
-});
-
-*/
-
-// document.querySelector(".todos-list").addEventListener("click", function (e) {
-//   e.preventDefault();
-
-//   if(e.target.classList.contains('checkbox')){
-//     if(e.target.cheked){
-//       console.log('it is checked')
-//     } else {
-//       console.log('Not checked');
-//     }
-//   }
-// })
